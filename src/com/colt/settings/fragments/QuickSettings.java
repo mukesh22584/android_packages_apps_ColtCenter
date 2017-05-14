534d36f2a0e56f10e6f2a9bc73af3bc6b55a71ef
RakeshBatra <raakesh.batra@rediffmail.com>
2017-05-14 04:42:22 +0000

    Custom QS rows/columns [2/2]
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/colt/settings/fragments/QuickSettings.java
+++ src/com/colt/settings/fragments/QuickSettings.java
@@ -3,7 +3,6 @@ package com.colt.settings.fragments;
 import android.os.Bundle;
 import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
@@ -24,8 +23,14 @@ public class QuickSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+    private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
+    private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
+    private static final String PREF_COLUMNS = "qs_columns";
 
     private CustomSeekBarPreference mSysuiQqsCount;
+    private CustomSeekBarPreference mRowsPortrait;
+    private CustomSeekBarPreference mRowsLandscape;
+    private CustomSeekBarPreference mQsColumns;
 
     @Override
     protected int getMetricsCategory() {
@@ -38,24 +43,65 @@ public class QuickSettings extends SettingsPreferenceFragment
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.quick_settings);
+
 	ContentResolver resolver = getActivity().getContentResolver();
+
 
         mSysuiQqsCount = (CustomSeekBarPreference) findPreference(PREF_SYSUI_QQS_COUNT);
         int SysuiQqsCount = Settings.Secure.getInt(resolver,
                Settings.Secure.QQS_COUNT, 5);
 	mSysuiQqsCount.setValue(SysuiQqsCount);
 	mSysuiQqsCount.setOnPreferenceChangeListener(this);
+
+	 mRowsPortrait = (CustomSeekBarPreference) findPreference(PREF_ROWS_PORTRAIT);
+        int rowsPortrait = Settings.Secure.getInt(resolver,
+                Settings.Secure.QS_ROWS_PORTRAIT, 3);
+        mRowsPortrait.setValue(rowsPortrait);
+        mRowsPortrait.setOnPreferenceChangeListener(this);
+
+        int defaultValue = getResources().getInteger(com.android.internal.R.integer.config_qs_num_rows_landscape_default);
+        mRowsLandscape = (CustomSeekBarPreference) findPreference(PREF_ROWS_LANDSCAPE);
+        int rowsLandscape = Settings.Secure.getInt(resolver,
+                Settings.Secure.QS_ROWS_LANDSCAPE, defaultValue);
+        mRowsLandscape.setValue(rowsLandscape);
+        mRowsLandscape.setOnPreferenceChangeListener(this);
+
+        mQsColumns = (CustomSeekBarPreference) findPreference(PREF_COLUMNS);
+        int columnsQs = Settings.Secure.getInt(resolver,
+                Settings.Secure.QS_COLUMNS, 3);
+        mQsColumns.setValue(columnsQs);
+	mQsColumns.setOnPreferenceChangeListener(this);
+
         }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+
+	int intValue;
+	int index;
+
         if (preference == mSysuiQqsCount) {
 	    int SysuiQqsCountValue = (Integer) newValue;
             Settings.Secure.putInt(resolver,
                    Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
             return true;
-        }
+        } else if (preference == mRowsPortrait) {
+            intValue = (Integer) newValue;
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.QS_ROWS_PORTRAIT, intValue);
+            return true;
+        } else if (preference == mRowsLandscape) {
+            intValue = (Integer) newValue;
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.QS_ROWS_LANDSCAPE, intValue);
+            return true;
+        } else if (preference == mQsColumns) {
+            intValue = (Integer) newValue;
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.QS_COLUMNS, intValue);
+            return true;
+	}
         return false;
     }
 }
