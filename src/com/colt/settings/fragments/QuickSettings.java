de8f43eafd150d07da15ee90da0ee617ee3dd3e7
RakeshBatra <raakesh.batra@rediffmail.com>
2017-05-14 03:20:25 +0000

    Custom small QS tiles [2/2]
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/colt/settings/fragments/QuickSettings.java
+++ src/com/colt/settings/fragments/QuickSettings.java
@@ -1,51 +1,61 @@
 package com.colt.settings.fragments;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
 import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
 import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 
+import com.android.settings.R;
+import com.colt.settings.preferences.CustomSeekBarPreference;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
-import java.util.List;
-import java.util.ArrayList;
-
-public class QuickSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.quick_settings);
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 
-        }
+public class QuickSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
 
-        return false;
-    }
+    private CustomSeekBarPreference mSysuiQqsCount;
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.COLT;
     }
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.quick_settings);
+	ContentResolver resolver = getActivity().getContentResolver();
+
+        mSysuiQqsCount = (CustomSeekBarPreference) findPreference(PREF_SYSUI_QQS_COUNT);
+        int SysuiQqsCount = Settings.Secure.getInt(resolver,
+               Settings.Secure.QQS_COUNT, 5);
+	mSysuiQqsCount.setValue(SysuiQqsCount);
+	mSysuiQqsCount.setOnPreferenceChangeListener(this);
+        }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSysuiQqsCount) {
+	    int SysuiQqsCountValue = (Integer) newValue;
+            Settings.Secure.putInt(resolver,
+                   Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+            return true;
+        }
+        return false;
+    }
 }
