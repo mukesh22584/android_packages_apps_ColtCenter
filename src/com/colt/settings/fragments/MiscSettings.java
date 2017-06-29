e4c83a3df7af1aab6f230a1966d5b0f2fbdf358d
RakeshBatra <raakesh.batra@rediffmail.com>
2017-06-29 14:24:13 +0000

    Dashboard Settings Deck
    Thanks to all the authors below
    
    commit 6866d5a53bb1742697641859dfb93bdb52eaf57f
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Sun Jan 22 09:39:19 2017 +0100
    
        Rework dashboard settings [1/2]
    
        - Get rid of the list preference, use seekbar because is sexy af
        - Keep portrait/landscape column count apart
        - Rename strings, no need to say dashboard every time
        - Removed summaries double lines toggle, do it based on the condition
        - Correct summary switch logic
        - Fix integer, now working for themes (tested it on Sublime, thanks @xwasabi)
    
    commit 7fb4f8043b9457b0a2f7e8b0abae9ed7504ee618
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Sat Jan 21 10:21:19 2017 +0100
    
        Dashboard summaries double lines toggle [2/2]
    
        Inspired by @xWASABI and his commit here
        TeamTwisted/packages_apps_Settings@3c62ed2
    
        This would come in handy for tablets that use 2 rows in the dashboard and for when
        translations are just too big to fit in 1 line or bigger DPI
    
        - Removed background color on the main dashboard, looks better when applying
        2/3 columns and does not break any themes so win win
    
    commit 276194c8b25beeef181ed106c16e1b71050b12ce
    Author: maxwen <max.weninger@gmail.com>
    Date:   Sat Jan 21 10:00:31 2017 +0100
    
        Dashboard tile layout config [1/2]
    
        add config to hide summary and use 2 columns
        always show one more column in landscape mode
    
        *DU Edits*
    
        - Replaced menu option to how we had it in M
        DirtyUnicorns/android_packages_apps_Settings@8bfc300
    
        - Added back integer so Substratum themers can overwrite this with themes
    
    commit 4c616c625a7e4efb56ed9ec47dbb927755b63fd9
    Author: xyyx <xyyx@mail.ru>
    Date:   Thu Sep 15 16:23:33 2016 -0400
    
        Disable dashboard conditions [2/2]
    
        Change-Id: I8843d7d8a6e37047c40abf70fa2b244b95925277
    
    commit f0028ed23b208eeacdaa65b448087850d3d214ab
    Author: Yasir <saeedyasir91@gmail.com>
    Date:   Wed Sep 14 08:12:20 2016 -0400
    
        Improvements to dashboard suggestions
    
        - Change the logic
    
        http://i.imgur.com/OJv71IN.png
        http://i.imgur.com/ylojEgY.png
    
        Change-Id: I6e3f47ecb7a54f51a84b70bae0cdb16d5aef6731
    
    commit 3879c6b0f70a8da6b647a01127af8532b9cc93f0
    Author: Alex <mazdarider23@gmail.com>
    Date:   Mon Sep 12 17:52:36 2016 -0400
    
        Remove dashboard tile summaries [1/2]
    
        Change-Id: I6fab69dc56b29762add78fa7b118a63b8f08b33c
    
    commit 373e28d9ce6841016ed9e539e56337692e1c5a99
    Author: Alex <mazdarider23@gmail.com>
    Date:   Mon Sep 12 17:52:36 2016 -0400
    
        Disable dashboard suggestions [1/2]
    
        Change-Id: I6fab69dc56b29762add78fa7b118a63b8f08b32c
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/colt/settings/fragments/MiscSettings.java
+++ src/com/colt/settings/fragments/MiscSettings.java
@@ -44,15 +44,61 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.dashboard.DashboardSummary;
 
-public class MiscSettings extends SettingsPreferenceFragment {
+import com.settings.preferences.CustomSeekBarPreference;
+
+import java.util.Date;
+
+
+public class MiscSettings extends SettingsPreferenceFragment
+         implements Preference.OnPreferenceChangeListener {
+
+
+    private static final String DASHBOARD_PORTRAIT_COLUMNS = "dashboard_portrait_columns";
+    private static final String DASHBOARD_LANDSCAPE_COLUMNS = "dashboard_landscape_columns";
+
+
+	private CustomSeekBarPreference mDashboardPortraitColumns;
+	private CustomSeekBarPreference mDashboardLandscapeColumns;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.misc_settings);
-        PreferenceScreen prefScreen = getPreferenceScreen();
+	ContentResolver resolver = getActivity().getContentResolver();
+
+        mDashboardPortraitColumns = (CustomSeekBarPreference) findPreference(DASHBOARD_PORTRAIT_COLUMNS);
+        int columnsPortrait = Settings.System.getInt(resolver,
+                Settings.System.DASHBOARD_PORTRAIT_COLUMNS, DashboardSummary.mNumColumns);
+        mDashboardPortraitColumns.setValue(columnsPortrait / 1);
+        mDashboardPortraitColumns.setOnPreferenceChangeListener(this);
+
+        mDashboardLandscapeColumns = (CustomSeekBarPreference) findPreference(DASHBOARD_LANDSCAPE_COLUMNS);
+        int columnsLandscape = Settings.System.getInt(resolver,
+                Settings.System.DASHBOARD_LANDSCAPE_COLUMNS, 2);
+        mDashboardLandscapeColumns.setValue(columnsLandscape / 1);
+        mDashboardLandscapeColumns.setOnPreferenceChangeListener(this);
     }
+
+
+     @Override
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+     ContentResolver resolver = getActivity().getContentResolver();
+	if (preference == mDashboardPortraitColumns) {
+	int columnsPortrait = (Integer) newValue;
+            Settings.System.putInt(resolver, Settings.System.DASHBOARD_PORTRAIT_COLUMNS, columnsPortrait * 1);
+            return true;
+        }else if (preference == mDashboardLandscapeColumns) {
+            int columnsLandscape = (Integer) newValue;
+            Settings.System.putInt(resolver, Settings.System.DASHBOARD_LANDSCAPE_COLUMNS, columnsLandscape * 1);
+            return true;
+
+	}
+         return false;
+     }
 
     @Override
     protected int getMetricsCategory() {
