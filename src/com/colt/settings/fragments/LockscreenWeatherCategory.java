42dfdd05133def6bf4cc8cd7810f11e848a793ab
RakeshBatra <raakesh.batra@rediffmail.com>
2017-07-21 02:43:58 +0000

    Add lockscreen weather [2/2]
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/colt/settings/fragments/LockscreenWeatherCategory.java
+++ src/com/colt/settings/fragments/LockscreenWeatherCategory.java
@@ -51,130 +51,22 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
-public class LockscreenWeatherCategory extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class LockscreenWeatherCategory extends SettingsPreferenceFragment {
 
+	private static final String TAG = "LockScreenWeather";
 
-    private static final String PREF_CONDITION_ICON =
-            "weather_condition_icon";
-    private static final String PREF_HIDE_WEATHER =
-            "weather_hide_panel";
-    private static final String PREF_NUMBER_OF_NOTIFICATIONS =
-            "weather_number_of_notifications";
-    private static final String KEY_LOCK_CLOCK =
-            "lock_clock";
-
-    private static final int MONOCHROME_ICON = 0;
-
-    private ListPreference mConditionIcon;
-    private ListPreference mHideWeather;
-    private CustomSeekBarPreference mNumberOfNotifications;
-
-    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
 
-        addPreferencesFromResource(R.xml.lockscreen_weather);
+        addPreferencesFromResource(R.xml.lock_screen_weather);
 
-        mResolver = getActivity().getContentResolver();
-        PreferenceScreen prefs = getPreferenceScreen();
-
-        // Remove the lock clock preference if its not installed
-        if (!isPackageInstalled("com.cyanogenmod.lockclock")) {
-            removePreference(KEY_LOCK_CLOCK);
         }
-		
-        mConditionIcon =
-                (ListPreference) findPreference(PREF_CONDITION_ICON);
-        int conditionIcon = Settings.System.getInt(mResolver,
-               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
-        mConditionIcon.setValue(String.valueOf(conditionIcon));
-        mConditionIcon.setSummary(mConditionIcon.getEntry());
-        mConditionIcon.setOnPreferenceChangeListener(this);
-
-        mHideWeather =
-                (ListPreference) findPreference(PREF_HIDE_WEATHER);
-        int hideWeather = Settings.System.getInt(mResolver,
-               Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, 0);
-        mHideWeather.setValue(String.valueOf(hideWeather));
-        mHideWeather.setOnPreferenceChangeListener(this);
-
-        mNumberOfNotifications =
-                (CustomSeekBarPreference) findPreference(PREF_NUMBER_OF_NOTIFICATIONS);
-        int numberOfNotifications = Settings.System.getInt(mResolver,
-                Settings.System.LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS, 4);
-        mNumberOfNotifications.setValue(numberOfNotifications);
-        mNumberOfNotifications.setOnPreferenceChangeListener(this);
-
-        updatePreference();
-
-    }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.COLT;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        updatePreference();
-    }
-
-    private void updatePreference() {
-        int hideWeather = Settings.System.getInt(mResolver,
-                Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, 0);
-        if (hideWeather == 0) {
-            mNumberOfNotifications.setEnabled(false);
-            mHideWeather.setSummary(R.string.weather_hide_panel_auto_summary);
-        } else if (hideWeather == 1) {
-            mNumberOfNotifications.setEnabled(true);
-            mHideWeather.setSummary(R.string.weather_hide_panel_custom_summary);
-        } else {
-            mNumberOfNotifications.setEnabled(false);
-            mHideWeather.setSummary(R.string.weather_hide_panel_never_summary);
-        }
-      }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mConditionIcon) {
-            int intValue = Integer.valueOf((String) newValue);
-            int index = mConditionIcon.findIndexOfValue((String) newValue);
-            Settings.System.putInt(mResolver,
-                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
-            mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
-            return true;
-        } else if (preference == mHideWeather) {
-            int intValue = Integer.valueOf((String) newValue);
-            int index = mHideWeather.findIndexOfValue((String) newValue);
-            Settings.System.putInt(mResolver,
-                    Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, intValue);
-            updatePreference();
-            return true;
-        } else if (preference == mNumberOfNotifications) {
-            int numberOfNotifications = (Integer) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS,
-            numberOfNotifications);
-            return true;
-        }
-        return false;
-    }
-	
-    private boolean isPackageInstalled(String packageName) {
-        PackageManager pm = getPackageManager();
-        boolean installed = false;
-        try {
-           pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
-           installed = true;
-        } catch (PackageManager.NameNotFoundException e) {
-           installed = false;
-        }
-        return installed;
-    }
 }
